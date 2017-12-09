7ca3809d1c8f316c7c89bca65efb8b55734b3c1e
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-09 20:48:10 +0000

    Added Film class with save method, including console.rb code to create and save a film

--- console.rb
+++ console.rb
@@ -1,8 +1,12 @@
 require('pry-byebug')
 require_relative('./models/customer.rb')
+require_relative('./models/film.rb')
 
 customer1 = Customer.new({'name' => 'Dave Ellis', 'funds' => '50'})
 customer1.save()
+
+film1 = Film.new({'title' => 'Star Wars: The Last Jedi', 'price' => '10'})
+film1.save()
 
 
 binding.pry
