860b95b489901687baca5a41f1df92d39b5c9813
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-09 22:27:11 +0000

    Added .films method to Customer class

--- console.rb
+++ console.rb
@@ -27,6 +27,13 @@ ticket2 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film2.id})
 ticket2.save()
 ticket3 = Ticket.new({'customer_id' => customer3.id, 'film_id' => film3.id})
 ticket3.save()
+ticket4 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film2.id})
+ticket4.save()
+ticket5 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film3.id})
+ticket5.save()
+ticket6 = Ticket.new({'customer_id' => customer3.id, 'film_id' => film1.id})
+ticket6.save()
+
 
 binding.pry
 nil
--- models/customer.rb
+++ models/customer.rb
@@ -29,6 +29,17 @@ class Customer
     return result
   end
 
+  def films()
+    sql = "SELECT films.* FROM films
+    INNER JOIN tickets
+    ON films.id = tickets.film_id
+    WHERE tickets.customer_id = $1"
+    values = [@id]
+    film_hashes = SqlRunner.run(sql, values)
+    films = film_hashes.map{|film| Film.new(film)}
+    return films
+  end
+
   #Update
   def update()
     sql= "UPDATE customers SET (name, funds) = ($1, $2)
