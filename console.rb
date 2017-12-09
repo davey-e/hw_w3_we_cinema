e73300bc18fd92d0fccb14cdf8a0fafdef1fc6d3
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-09 20:59:16 +0000

    Added Ticket class with save method, including console.rb code to create and save a ticket

--- console.rb
+++ console.rb
@@ -1,12 +1,16 @@
 require('pry-byebug')
 require_relative('./models/customer.rb')
 require_relative('./models/film.rb')
+require_relative('./models/ticket.rb')
 
 customer1 = Customer.new({'name' => 'Dave Ellis', 'funds' => '50'})
 customer1.save()
 
 film1 = Film.new({'title' => 'Star Wars: The Last Jedi', 'price' => '10'})
 film1.save()
+
+ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket1.save()
 
 
 binding.pry
